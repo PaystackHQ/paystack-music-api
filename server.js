60cf1fcafeb62e97a6aa2dbea9402de0ce39d5d6
Glitch (hello-express) <none>
2019-08-28 13:08:06 +0000

    🐁🍎 Checkpoint ./server.js:242684/682

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 
+const token = 'oxp-8842510723-138344650327-740974107989-8d11b1059a5b4e8bc9854fbc048dbc5f';
+const channelID = 'C7JECTX2T';
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -15,7 +18,11 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   // response.sendFile(__dirname + '/views/index.html');
-  axios.get()
+  const res = axios.get('https://slack.com/api/conversations.list?token=xoxp-8842510723-138344650327-740974107989-8d11b1059a5b4e8bc9854fbc048dbc5f')
+    .then(r => r);
+  // C7JECTX2T
+  console.log(res);
+  response.send(res);
 });
 
 // listen for requests :)
