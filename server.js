20a51b2dbfc8ba539767e47dc2d6a2a4fbed4ed2
Glitch (hello-express) <none>
2019-08-28 18:25:10 +0000

    👙🌶 Checkpoint ./server.js:242684/949

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 
-const token = 'oxp-8842510723-138344650327-740974107989-8d11b1059a5b4e8bc9854fbc048dbc5f';
-const channelID = 'C7JECTX2T';
+const TOKEN = 'xoxp-8842510723-138344650327-740974107989-8d11b1059a5b4e8bc9854fbc048dbc5f';
+const CHANNEL_ID = 'C7JECTX2T';
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -18,11 +18,16 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   // response.sendFile(__dirname + '/views/index.html');
-  const res = axios.get('https://slack.com/api/conversations.list?token=xoxp-8842510723-138344650327-740974107989-8d11b1059a5b4e8bc9854fbc048dbc5f')
-    .then(r => r);
+  const url = `https://slack.com/api/conversations.history?token=${TOKEN}&channel=${CHANNEL_ID}`;
+  axios.get(url)
+    .then(r => {
+      console.log(r);
+      response.end(r);
+    }).catch(error => {
+      response.send('error o');
+      console.log(error);
+    });
   // C7JECTX2T
-  console.log(res);
-  response.send(res);
 });
 
 // listen for requests :)
