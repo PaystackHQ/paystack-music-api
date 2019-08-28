e901b4190fe844aa537eae84f9df9dd54fa1e610
Glitch (hello-express) <none>
2019-08-28 19:08:16 +0000

    🔋🧀 Checkpoint ./server.js:242684/229 ./package.json:242684/1329

--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const moment = require('moment');
+
+const startTime = moment('2019-08-01').startOf('day').format('x.SSSSSS');
+
 
 const TOKEN = 'xoxp-8842510723-138344650327-740974107989-8d11b1059a5b4e8bc9854fbc048dbc5f';
 const CHANNEL_ID = 'C7JECTX2T';
@@ -18,13 +22,14 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   // response.sendFile(__dirname + '/views/index.html');
-  const url = `https://slack.com/api/conversations.history?token=${TOKEN}&channel=${CHANNEL_ID}`;
+  const url = `https://slack.com/api/conversations.history?token=${TOKEN}&channel=${CHANNEL_ID}&oldest=${startTime}`;
   axios.get(url)
     .then(r => {
-      console.log(r);
-      response.end(r);
-    }).catch(error => {
-      response.send('error o');
+      console.log(r.data);
+      response.send(r.data, null, 3);
+    })
+    .catch(error => {
+      response.send(JSON.stringify(error));
       console.log(error);
     });
   // C7JECTX2T
