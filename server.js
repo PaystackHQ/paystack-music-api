f0f09f63bb4d76d00e693c3b580435ef2f557a87
Glitch (slackurate-cress) <none>
2019-09-03 03:36:17 +0000

    🌚🌒 Checkpoint ./server.js:242684/686

--- server.js
+++ server.js
@@ -25,14 +25,29 @@ app.get('/', async function(request, response) {
 });
 
 app.get('/authorize', async function(request, response) {
-  // response.sendFile(__slack + '/vChannelindex.html');
-  // const authURL = spotify.createAuthURL();
+  const authURL = spotify.createAuthURL();
   const html = `
     <!DOCTYPE html>
     <html>
       <head></head>
       <body>
         <h1>Hello World!</h1>
+        <a target="_blank" href="${authURL}">Click here to authorize</a>
+      </body>
+    </html>
+  `;
+  response.send(html); 
+});
+
+app.get('/callback', async function(request, response) {
+  // response.sendFile(__slack + '/vChannelindex.html');
+  
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head></head>
+      <body>
+        <h1>All done!</h1>
       </body>
     </html>
   `;
