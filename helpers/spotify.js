8ea87a386df021380ec35c7905ed728893460a2d
Glitch (slackurate-cress) <none>
2019-09-03 03:24:17 +0000

    🚔🛤 Checkpoint ./helpers/spotify.js:242684/175 ./server.js:242684/2086

--- helpers/spotify.js
+++ helpers/spotify.js
@@ -5,3 +5,11 @@ const spotifyApi = new SpotifyWebApi({
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
   redirectUri: process.env.SPOTIFY_REDIRECT_URI
 });
+
+const scopes = ['playlist-modify-public'];
+
+module.exports = {
+  createAuthURL() {
+    return spotifyApi.createAuthorizeURL(scopes);
+  }
+};
--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require('express');
 const app = express();
 const slack = require('./helpers/slack');
+const spotify = require('./helpers/spotify');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.om/en/starter/static-files.html
@@ -21,6 +22,21 @@ app.get('/', async function(request, response) {
   } catch (error) {
     response.send("An error occurred\n\n" + error); 
   }
+});
+
+app.get('/authorize', async function(request, response) {
+  // response.sendFile(__slack + '/vChannelindex.html');
+  // const authURL = spotify.createAuthURL();
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head></head>
+      <body>
+        <h1>Hello World!</h1>
+      </body>
+    </html>
+  `;
+  response.send(html); 
 });
 
 // listen for requests :)
