afa81341fe0cde6cc00e214ee3a755a8f25aa9b9
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2019-09-23 23:59:16 +0100

    Run puppeteer without sandbox

--- helpers/image.js
+++ helpers/image.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   async generateCoverImage({ color, month, year }) {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] }); // only doing this cos I'm loading a local file I trust
     const page = await browser.newPage();
     await page.setViewport({ width: 640, height: 640, deviceScaleFactor: 4 });
     await page.goto(`file://${path.join(process.cwd() + '/views/covers.html')}?color=${color.replace('#', '%23')}&month=${month}&year=${year}`);
