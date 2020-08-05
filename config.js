f7e53e96155f5785081132797198f3de57df509d
Ifeoluwa Sobogun <sobogunifeoluwa@gmail.com>
2020-08-05 12:06:38 +0100

    chore(centralize-envs): have all environment variables accessible from a central config file

---
+++ config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  app: {
+    port: process.env.PORT,
+    triggerUri: process.env.APP_TRIGGER_URI,
+  },
+  db: {
+    uri: process.env.DB_URI,
+  },
+  spotify: {
+    token: process.env.SPOTIFY_TOKEN,
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+    userId: process.env.SPOTIFY_USER_ID,
+    tokenSecret: process.env.TOKEN_SECRET,
+  },
+  slack: {
+    token: process.env.SLACK_TOKEN,
+    sourceChannel: process.env.SLACK_SOURCE_CHANNEL,
+    targetChannel: process.env.SLACK_TARGET_CHANNEL,
+  },
+};
