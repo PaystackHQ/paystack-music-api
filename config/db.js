232bce7d5978ea0541774333e1ae8d64c02f3469
opeyemi-paystack <opeyemi@paystack.com>
2020-08-15 08:57:51 +0100

    add graceful shutdown for mongo

--- config/db.js
+++ config/db.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose');
 const { db: { uri: databaseURI } } = require('./index');
 const logger = require('../helpers/logger');
+const { gracefulShutdown } = require('../helpers/util');
 
 mongoose.connect(databaseURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongoose.connection;
-db.on('open', () => {
+const conn = mongoose.connection;
+
+conn.on('open', () => {
   logger.info('Database Connected');
-})
-  .on('error', (err) => {
-    logger.error(`Connection error: ${err.message}`);
+});
+
+conn.on('error', (err) => {
+  logger.error(`Connection error: ${err.message}`);
+});
+
+// graceful shutdown for nodemon restarts
+process.once('SIGUSR2', () => {
+  gracefulShutdown(conn, 'nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
   });
+});
+
+// graceful shutdown for app termination
+process.on('SIGINT', () => {
+  gracefulShutdown(conn, 'app termination', () => {
+    process.exit(0);
+  });
+});
