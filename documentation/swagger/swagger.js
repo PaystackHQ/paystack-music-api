d0ed9bfa5456b864c967707279391a314ad551bc
Ifeoluwa Sobogun <sobogunifeoluwa@gmail.com>
2021-08-20 17:02:59 +0100

    :bulb: chore(updating-swagger-json): updating swagger json file

--- documentation/swagger/swagger.js
+++ documentation/swagger/swagger.js
@@ -2,11 +2,11 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Hello World',
+      title: 'Paystack Music API',
       version: '1.0.0',
     },
   },
-  apis: ['./routes/*.js'], // files containing annotations as above
+  apis: ['./routes/*.js'],
 };
 
 module.exports = options;
